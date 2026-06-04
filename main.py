ff433532a134d12115c642430c8c7695e855210c
harry01274 <harheron@gmail.com>
2026-06-04 23:18:33 +0100

    E: Create player class, update constants, and render ship in main

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
-    # 1. Print starting messages
+    # Print starting messages
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # 2. Initialise all imported pygame modules
+    # Initialise all imported pygame modules
     pygame.init()
 
-    # 3. Initialise the window/screen for display
+    # Initialise the window/screen for display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # 5. Create clock to limit FPS
+    # Create clock to limit FPS
     clock = pygame.time.Clock()
     dt = 0.0
 
+    # Initiate Player in centre of screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    # 4. Create game loop - Drawing game to screen
+
+    # Create game loop - Drawing game to screen
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
+
+
 
 if __name__ == "__main__":
     main()
