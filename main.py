a539e04bc3195723d143c62853d1d1e14d32ce33
harry01274 <harheron@gmail.com>
2026-06-05 21:14:27 +0100

    H: Created groups for player class, updatable and draw

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    # Create Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     # Initiate Player in centre of screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -29,9 +35,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
