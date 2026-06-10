f7c38c05ac05a4361fca85f187cbb8d6e21ce612
harry01274 <harheron@gmail.com>
2026-06-10 15:02:01 +0100

    L: Created Shot class and gave player method shoot

--- circleshape.py
+++ circleshape.py
@@ -23,6 +23,5 @@ class CircleShape(pygame.sprite.Sprite):
         # must override
         pass
 
-    def collides_with(self, other) -> bool:
-        distance = pygame.math.Vector2.distance_to(self.position, other.position)
-        return distance <= (self.radius + other.radius)
+    def collides_with(self, other: "CircleShape") -> bool:
+        return self.position.distance_to(other.position) <= self.radius + other.radius
